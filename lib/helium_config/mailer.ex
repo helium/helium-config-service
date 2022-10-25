014eeba273c68c49f8cdfd90f39d9e08555aa937
Brandon Phillips <brandon@nova-labs.com>
2022-10-25 12:18:51 -0700

    Remove the old code.

--- lib/helium_config/mailer.ex
+++
@@ -1,5 +0,0 @@
-defmodule HeliumConfig.Mailer do
-  use Swoosh.Mailer, otp_app: :helium_config
-
-  @moduledoc false
-end
--- lib/helium_config/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule HeliumConfig.Repo do
-  use Ecto.Repo,
-    otp_app: :helium_config,
-    adapter: Ecto.Adapters.Postgres
-end
