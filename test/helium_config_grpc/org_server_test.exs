dc7a33bfa002d1c292106f795e064d11b156939a
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-11-22 11:19:43 -0700

    sign the requests now that auth is enabled in these tests

--- test/helium_config_grpc/org_server_test.exs
+++ test/helium_config_grpc/org_server_test.exs
@@ -36,17 +36,20 @@ defmodule HeliumConfigGRPC.OrgServerTest do
 
   describe "create" do
     test "roaming org" do
-      %{public: owner_pubkey} = HeliumConfig.Core.Crypto.generate_key_pair()
+      %{public: owner_pubkey, secret: owner_secret} = HeliumConfig.Core.Crypto.generate_key_pair()
       %{public: payer_pubkey} = HeliumConfig.Core.Crypto.generate_key_pair()
+      :ok = store_pubkey_as_admin_key(owner_pubkey)
 
       req =
-        ConfigProto.OrgCreateRoamerReqV1.new(%{
-          owner: HeliumConfig.Core.Crypto.pubkey_to_bin(owner_pubkey),
-          payer: HeliumConfig.Core.Crypto.pubkey_to_bin(payer_pubkey),
-          timestamp: HeliumConfig.Fixtures.utc_now_msec(),
-          net_id: 0xC00053,
-          # TODO signature
-        })
+        sign_request(
+          owner_secret,
+          ConfigProto.OrgCreateRoamerReqV1.new(%{
+            owner: HeliumConfig.Core.Crypto.pubkey_to_bin(owner_pubkey),
+            payer: HeliumConfig.Core.Crypto.pubkey_to_bin(payer_pubkey),
+            timestamp: HeliumConfig.Fixtures.utc_now_msec(),
+            net_id: 0xC00053
+          })
+        )
 
       {:ok, channel} = GRPC.Stub.connect("localhost:50051")
       result = ConfigProto.Org.Stub.create_roamer(channel, req)
@@ -66,16 +69,19 @@ defmodule HeliumConfigGRPC.OrgServerTest do
     end
 
     test "helium org" do
-      %{public: owner_pubkey} = HeliumConfig.Core.Crypto.generate_key_pair()
+      %{public: owner_pubkey, secret: owner_secret} = HeliumConfig.Core.Crypto.generate_key_pair()
       %{public: payer_pubkey} = HeliumConfig.Core.Crypto.generate_key_pair()
+      :ok = store_pubkey_as_admin_key(owner_pubkey)
 
       req =
-        ConfigProto.OrgCreateHeliumReqV1.new(%{
-          owner: HeliumConfig.Core.Crypto.pubkey_to_bin(owner_pubkey),
-          payer: HeliumConfig.Core.Crypto.pubkey_to_bin(payer_pubkey),
-          timestamp: HeliumConfig.Fixtures.utc_now_msec()
-          # TODO signature
-        })
+        sign_request(
+          owner_secret,
+          ConfigProto.OrgCreateHeliumReqV1.new(%{
+            owner: HeliumConfig.Core.Crypto.pubkey_to_bin(owner_pubkey),
+            payer: HeliumConfig.Core.Crypto.pubkey_to_bin(payer_pubkey),
+            timestamp: HeliumConfig.Fixtures.utc_now_msec()
+          })
+        )
 
       {:ok, channel} = GRPC.Stub.connect("localhost:50051")
       result = ConfigProto.Org.Stub.create_helium(channel, req)
@@ -101,13 +107,17 @@ defmodule HeliumConfigGRPC.OrgServerTest do
       %{public: owner_pubkey} = HeliumConfig.Core.Crypto.generate_key_pair()
       %{public: payer_pubkey} = HeliumConfig.Core.Crypto.generate_key_pair()
 
+      # This is being signed with the previous owners key because it's been
+      # inserted as an admin key.
       req =
-        ConfigProto.OrgCreateHeliumReqV1.new(%{
-          owner: HeliumConfig.Core.Crypto.pubkey_to_bin(owner_pubkey),
-          payer: HeliumConfig.Core.Crypto.pubkey_to_bin(payer_pubkey),
-          timestamp: HeliumConfig.Fixtures.utc_now_msec()
-          # TODO signature
-        })
+        sign_request(
+          owner_secret,
+          ConfigProto.OrgCreateHeliumReqV1.new(%{
+            owner: HeliumConfig.Core.Crypto.pubkey_to_bin(owner_pubkey),
+            payer: HeliumConfig.Core.Crypto.pubkey_to_bin(payer_pubkey),
+            timestamp: HeliumConfig.Fixtures.utc_now_msec()
+          })
+        )
 
       {:ok, channel} = GRPC.Stub.connect("localhost:50051")
       result = ConfigProto.Org.Stub.create_helium(channel, req)
@@ -116,7 +126,6 @@ defmodule HeliumConfigGRPC.OrgServerTest do
       assert(%{__struct__: ConfigProto.OrgV1} = org_res.org)
       assert(0xC00053 == org_res.net_id)
       assert(1 == length(org_res.devaddr_ranges))
-
 
       assert([%{__struct__: ConfigProto.DevaddrRangeV1} = range] = org_res.devaddr_ranges)
       # NOTE: assuming default allocation of 8 devaddrs for now
@@ -132,5 +141,19 @@ defmodule HeliumConfigGRPC.OrgServerTest do
       |> HeliumConfig.create_organization!()
 
     Map.put(ctx, :valid_org, valid_org)
+  end
+
+  defp sign_request(secret_key, request) do
+    sig_fun = HeliumConfig.Core.Crypto.mk_sig_fun(secret_key)
+    encoded = ConfigProto.OrgCreateHeliumReqV1.encode(request)
+    %{request | signature: sig_fun.(encoded)}
+  end
+
+  defp store_pubkey_as_admin_key(pubkey) do
+    b58 = HeliumConfig.Core.Crypto.pubkey_to_b58(pubkey)
+    grpc_env = Application.get_env(:helium_config, HeliumConfigGRPC)
+    grpc_env = Keyword.put(grpc_env, :admin_keys, [b58])
+    Application.put_env(:helium_config, HeliumConfigGRPC, grpc_env)
+    :ok
   end
 end
